31877c08d5e0348f1670dcdec9d4cf6ab30505c2
cmmm976 <icdk5b4zf@relay.firefox.com>
2022-02-20 14:25:05 +0100

    [postingnewruns] fix autorisation + dockerization

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ bot = Bot(
     owner_ids=config["owners"], command_attrs=dict(hidden=True), help_command=HelpFormat(),
     allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=False),
     intents=discord.Intents(  # kwargs found at https://discordpy.readthedocs.io/en/latest/api.html?highlight=intents#discord.Intents
-        guilds=True, members=True, messages=True, reactions=True, presences=True
+        guilds=True, members=True, messages=True, reactions=True, presences=True, message_content=True
     )
 )
 
@@ -27,7 +27,7 @@ for file in os.listdir("cogs"):
 
 @tasks.loop(seconds=10)
 async def post_new_runs():
-    channel = bot.get_channel(931584454156230687)
+    new_runs_channel = bot.get_channel(931584454156230687)
 
     newest_run = ''
     with open('test.json') as f:
@@ -39,25 +39,25 @@ async def post_new_runs():
     if  new_call != newest_run:
         print("New run !")
         newest_run = new_call
-        with open('test.json', 'w') as outfile:
+        with open('newest.json', 'w') as outfile:
             json.dump({"newest_run": newest_run}, outfile)
         
         embed_run = discord.Embed.from_dict(dict(fields=[{"name": key, "value": newest_run[key], "inline": True} for key in newest_run]))
 
-        runner_is_in_server = discord.utils.get(channel.guild.members, name=newest_run["Runner"]) != None
+        runner_is_in_server = discord.utils.get(new_runs_channel.guild.members, name=newest_run["Runner"]) != None
         
         if runner_is_in_server:
-            await channel.send(
+            await new_runs_channel.send(
                 "**A new run has been verified !**\n"
-                "GG {} for PB ! :partying_face:".format(discord.utils.get(channel.guild.members, name=newest_run["Runner"])), embed=embed_run
+                "GG {} for PB ! :partying_face:".format(discord.utils.get(new_runs_channel.guild.members, name=newest_run["Runner"])), embed=embed_run
             )
         else:
-            await channel.send(
+            await new_runs_channel.send(
                 "**A new run has been verified !**\n"
                 "GG **{}** for PB ! :partying_face:".format(newest_run["Runner"]), embed=embed_run
             )
             
-        await channel.send(newest_run["Video Link"])
+        await new_runs_channel.send(newest_run["Video Link"])
 
     
 
@@ -66,7 +66,7 @@ async def before_post_new_runs():
     await bot.wait_until_ready()
     print("Ready to post new runs")
 
-# post_new_runs.start()
+post_new_runs.start()
 
 try:
     bot.run(config["token"])
